37e080fcb59c97766ebf7a0fc705dca12d7d808e
Julie Buser <juliebuser1@Julies-MacBook-Pro.local>
2015-06-16 17:19:40 -0500

    JS first exercise

---
+++ public/js/if_else.js
@@ -0,0 +1,36 @@
+// ignore these lines for now
+// just know that the variable 'color' will end up with a random value from the colors array
+var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+var color = colors[Math.floor(Math.random()*colors.length)];
+console.log(color)
+var favorite = 'indigo'; // TODO: change this to your favorite color from the list
+var message = (color==favorite)? "Favorite" : "Not Favorite"
+console.log(message)
+
+if (color=='red') {
+    	console.log('red is the color of an apple');
+    }else if (color=='orange') {
+		console.log('orange is the color of a pumpkin');
+	}else if (color=='yellow') {
+		console.log('yellow is the color of a lemon');
+	}else if (color=='green') {
+		console.log('green is the color of grass');
+	}else if (color=='blue') {
+		console.log('blue is the color of the sky');
+	}else if (color=='indigo') {
+		console.log=('I do not know anything by that color');	
+	}else if (color=='violet') {
+		console.log=('I do not know anything by that color');
+	}
+
+
+
+// TODO: Create a block of if/else statements to check for every color except indigo and violet.
+// TODO: When a color is encountered log a message that tells the color, and an object of that color.
+//       Example: Blue is the color of the sky.
+
+// TODO: Have a final else that will catch indigo and violet.
+// TODO: In the else, log: I do not know anything by that color.
+
+// TODO: Using the ternary operator, conditionally log a statement that
+//       says whether the random color matches your favorite color.
