4a67f52fa5a52a8fd9ddfbbf7e1041be5d49abda
Julie Buser <jbuser2013@gmail.com>
2015-07-21 10:12:51 -0500

    page controller extract exercise

--- public/server-name-generator.php
+++ public/server-name-generator.php
@@ -1,13 +1,22 @@
-<?php
-	 
-$nouns = ['Cat', 'Kitten', 'Dog', 'Puppy', 'Panda', 'Giraffe', 'Cheetah', 'Meerkat', 'Whale', 'Gecko'];
-$adjectives = ['Furry', 'Cute', 'Cuddly', 'Peppy', 'Aggressive', 'Meek', 'Menacing', 'Mischievous', 'Frisky', 'Baby'];
+<?
+
+ /*The pageController function handles all processing for this page.
+ @return array An associative array of data to be used in rendering the html view.*/
+function pageController(){
+	$nouns = ['Cat', 'Kitten', 'Dog', 'Puppy', 'Panda', 'Giraffe', 'Cheetah', 'Meerkat', 'Whale', 'Gecko'];
+	$adjectives = ['Furry', 'Cute', 'Cuddly', 'Peppy', 'Aggressive', 'Meek', 'Menacing', 'Mischievous', 'Frisky', 'Baby'];
+
+	$data =[];
+	$data['nounAdjectivePair'] = getRandom($adjectives) . " " . getRandom($nouns);
+	return $data;
+};
 
 function getRandom($array){
 	$randomVariable = mt_rand(0, count($array));
 	return $array[$randomVariable];
 };
-$nounAdjectivePair = getRandom($adjectives) . " " . getRandom($nouns);
+/*Call the pageController funcion and extract all the returned array as local variables*/
+extract(pageController());
 
 ?>
 
@@ -21,6 +30,6 @@ $nounAdjectivePair = getRandom($adjectives) . " " . getRandom($nouns);
 </head>
 <body>
 	<h1>Server Name:</h1>
-	<h2><?php echo $nounAdjectivePair ?></h2>
+	<h2><?php echo $nounAdjectivePair?></h2>
 </body>
 </html>
