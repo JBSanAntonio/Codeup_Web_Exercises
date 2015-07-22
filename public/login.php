94c0fd8e73fb3a589d56a3da4d7a85248c1b5b46
Julie Buser <jbuser2013@gmail.com>
2015-07-22 16:01:19 -0500

    php POST login.php file

--- public/login.php
+++ public/login.php
@@ -1,13 +1,25 @@
 <?php
 
-	if (!empty($_POST)) {
+	session_start();
+
+/*	Add a check to the login.php page to see if the user is logged in and then
+ if they are, redirect to the authorized.php page instead of showing the login page
+*/
+	 function checklogin() {
+	  return (isset($_SESSION['login'])) ? true : false;
+	};
+
+	if(checklogin() === true){
+		header ("Location: authorize.php");
+	} elseif(!empty($_POST)) {
 		if ($_POST['username'] == 'guest' && $_POST['password'] == 'password') {
+			$_SESSION['LOGGED_IN_USER']=$_POST['username'];
 			header('location: authorized.php');
 			exit();
 		} else {
-			echo "Login Failed";
-		} 
-	};
+			echo "Login Failed"; 
+		}
+	} 
 ?>
 
 <!DOCTYPE html>
@@ -30,5 +42,11 @@
 </html>
 
 
+ 	<!-- if (empty($_SESSION['login']) {
+ 		header ("Location: authorize.php")
+ 	} -->
+
+ 	<!-- if (isset($_SESSION['login']) && $_SESSION['login'] != '') {
+	header ("Location: authorized.php"); -->
 
 
