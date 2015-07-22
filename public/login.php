c7bc2504a57b3d4aa88288faf3efb233cdaa4055
Julie Buser <jbuser2013@gmail.com>
2015-07-22 10:05:06 -0500

    added first stage POST username password form exercise

---
+++ public/login.php
@@ -0,0 +1,32 @@
+<?php
+
+	$username = isset($_POST['Username']) ? $_POST['Username'] : '';
+	$password = isset($_POST['password']) ? $_POST['password'] : '';
+
+	var_dump($_POST);
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>POST Example</title>
+</head>
+<body>
+	<h1>Login with Username and Password</h1>
+
+	 <form method="POST">
+        <label>username</label>
+        <input type="text" name="username"><br>
+        <label>password</label>
+        <input type="password" name="password"><br>
+        <input type="submit">
+    </form>
+    
+</body>
+</html>
+
+
+
+
