98a45048f81db6b9638fe7f2b0220330d0922301
Julie Buser <jbuser2013@gmail.com>
2015-07-23 12:30:04 -0500

    updated name adjective random pair generator

--- public/server-name-generator.php
+++ public/server-name-generator.php
@@ -1,12 +1,13 @@
 <?
 
  /*The pageController function handles all processing for this page.
- @return array An associative array of data to be used in rendering the html view.*/
+ @return associative array of data to be used in rendering the html view.*/
 function pageController(){
-	$nouns = ['Cat', 'Kitten', 'Dog', 'Puppy', 'Panda', 'Giraffe', 'Cheetah', 'Meerkat', 'Whale', 'Gecko'];
-	$adjectives = ['Furry', 'Cute', 'Cuddly', 'Peppy', 'Aggressive', 'Meek', 'Menacing', 'Mischievous', 'Frisky', 'Baby'];
+	$nouns = ['Cat', 'Kitten', 'Cuttlefish', 'Puppy', 'Panda', 'Giraffe', 'Cheetah', 'Meerkat', 'Whale', 'Gecko'];
+	$adjectives = ['Furry', 'Cute', 'Cuddly', 'Flamboyant', 'Aggressive', 'Meek', 'Menacing', 'Mischievous', 'Frisky', 'Baby'];
 
 	$data =[];
+	$data['nouns'] = ['Cat', 'Kitten', 'Dog', 'Puppy', 'Panda', 'Giraffe', 'Cheetah', 'Meerkat', 'Whale', 'Gecko'];
 	$data['nounAdjectivePair'] = getRandom($adjectives) . " " . getRandom($nouns);
 	return $data;
 };
@@ -15,7 +16,7 @@ function getRandom($array){
 	$randomVariable = mt_rand(0, count($array));
 	return $array[$randomVariable];
 };
-/*Call the pageController funcion and extract all the returned array as local variables*/
+/*Call the pageController function and extract all the returned array as local variables*/
 extract(pageController());
 
 ?>
