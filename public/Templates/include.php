beaf20a62f10a0de25cd76e563456959cc7151a7
Julie Buser <jbuser2013@gmail.com>
2015-07-24 14:03:13 -0500

    include.php exercise

---
+++ public/Templates/include.php
@@ -0,0 +1,16 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="/css/bootstrap.css">
+	<? include_once 'navBar.php'?>
+	<? include_once '../headerPHP.php'?>
+	<meta charset="utf-8">
+</head>
+<body>
+
+	<footer><? include_once '../footer.php'?>
+</body>
+
+</html>
