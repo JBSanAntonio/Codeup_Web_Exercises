4e3ef1d718bf1ca88df8a81b601e61de513e3f86
Julie Buser <jbuser2013@gmail.com>
2015-07-27 15:25:05 -0500

    revised authorized php for static PHP exercise

--- public/authorized.php
+++ public/authorized.php
@@ -1,11 +1,19 @@
 <?php
 	session_start();
 
-	/*Add a check to the authorized.php page to redirect to the login.php page if a user is not logged in. If they are, display their username after the authorized message.*/
+	require_once '../Auth.php';
+	require_once '../Input.php';
+	require_once '../log.php';
 
-	if(!(isset($_SESSION['LOGGED_IN_USER']))){
-		header ("Location: login.php");
+	session_start();
+
+	/*Add a check to the authorized.php page to redirect to the login.php page if a user is not logged in. If they are, display their username after the authorized message.*/
+	if(!Auth::check()){	
+		header ("Location:/login.php");
+	} else {
+		Log::logMessage('INFO', 'this info message is working');
 	}
+
 ?>
 
 <!DOCTYPE html>
@@ -14,11 +22,11 @@
     <title>POST Example Authorized</title>
 </head>
 <body>
-	
-	<h1>User is authorized</h1>
-	<h2>Username is: <?= $_SESSION['LOGGED_IN_USER'] ?></h2>
-<!-- Add link to authorized.php page that goes to logout.php.-->
+<!-- Use the Log class to log an info message: "User $username logged in."-->
+	<h1>User is authorized and logged in</h1>
+	<h2>User <?= $username?> . logged in.></h2>
 
+	<!-- Add link to authorized.php page that goes to logout.php.-->
 	<a href="logout.php">Logout</a>
 
 </body>
